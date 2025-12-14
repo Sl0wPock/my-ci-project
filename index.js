ea02c7fd311fe4b186d46acc70416eb0420ce439
Sl0wPock <100418905+Sl0wPock@users.noreply.github.com>
2025-12-14 12:38:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sum(a, b) {
 
 function main() {
   const result = sum(2, 3);
-  console.log(`Result is ${result}`);
+  console.log("CI test: everything is OK");
 }
 
 if (require.main === module) {
