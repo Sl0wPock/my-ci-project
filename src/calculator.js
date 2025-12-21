c2ce3c74cfdc0bdda1eb36edfc48343445fc8f80
Sl0wPock <100418905+Sl0wPock@users.noreply.github.com>
2025-12-21 15:21:22 +0300

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -1,13 +1,40 @@
+/**
+ * Калькулятор для выполнения математических операций
+ * @module calculator
+ */
 
-function multiply(a, b) {
-  return a * b;
-}
+/**
+ * Складывает два числа
+ * @param {number} a - Первое число
+ * @param {number} b - Второе число
+ * @returns {number} Сумма a и b
+ */
+export const add = (a, b) => a + b;
 
-function divide(a, b) {
-  if (b === 0) {
-    throw new Error('Division by zero');
-  }
+/**
+ * Вычитает второе число из первого
+ * @param {number} a - Первое число
+ * @param {number} b - Второе число
+ * @returns {number} Разница a и b
+ */
+export const subtract = (a, b) => a - b;
+
+/**
+ * Умножает два числа
+ * @param {number} a - Первое число
+ * @param {number} b - Второе число
+ * @returns {number} Произведение a и b
+ */
+export const multiply = (a, b) => a * b;
+
+/**
+ * Делит первое число на второе
+ * @param {number} a - Делимое
+ * @param {number} b - Делитель
+ * @returns {number} Результат деления a на b
+ * @throws {Error} Если b равно 0
+ */
+export const divide = (a, b) => {
+  if (b === 0) throw new Error('Cannot divide by zero');
   return a / b;
-}
-
-module.exports = { multiply, divide };
+};
