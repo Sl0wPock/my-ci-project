000440eeaf22d083f8f673277e65197edf820c4f
Sl0wPock <100418905+Sl0wPock@users.noreply.github.com>
2025-12-21 13:48:41 +0300

    Delete sum.test.js

--- sum.test.js
+++
@@ -1,9 +0,0 @@
-const { sum } = require("./index");
-
-test("adds 2 + 3 to equal 5", () => {
-  expect(sum(2, 3)).toBe(5);
-});
-
-test("adds negative numbers", () => {
-  expect(sum(-1, -2)).toBe(-3);
-});
