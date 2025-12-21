eab56e21f8f43040df66fdaecc8998e34ae6f8cf
Sl0wPock <100418905+Sl0wPock@users.noreply.github.com>
2025-12-21 15:21:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-function init() {
-  console.log('App initialized');
-}
+/**
+ * Главный модуль приложения
+ * @module index
+ */
 
-function getVersion() {
-  return '1.1.0';
-}
+import { add, subtract, multiply, divide } from './calculator.js';
 
-function add(a, b) {
-  return a + b;
-}
-
-module.exports = { init, getVersion, add };
+/**
+ * Экспортирует все функции калькулятора
+ */
+export { add, subtract, multiply, divide };
